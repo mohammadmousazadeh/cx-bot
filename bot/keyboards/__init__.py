d184a31af6d87c84638122f00e8ad9931db1cf1f
mohammadmousazadeh <82465346+mohammadmousazadeh@users.noreply.github.com>
2026-09-14 21:02:08 +0330

    Update __init__.py

--- bot/keyboards/__init__.py
+++ bot/keyboards/__init__.py
@@ -1,3 +1,3 @@
-from .main import get_cancel_kb, get_main_dashboard_kb
+from .main import get_cancel_kb, get_main_dashboard_kb, get_miniapp_inline_kb
 
-__all__ = ["get_main_dashboard_kb", "get_cancel_kb"]
+__all__ = ["get_main_dashboard_kb", "get_cancel_kb", "get_miniapp_inline_kb"]
